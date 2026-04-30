6b347373d136d6a8435e279e5acafcf0ceca9b9a
Pitágoras Alves <alves.pitagoras@gmail.com>
2026-04-30 10:38:36 -0300

    Updated configs for ministral 8b to fit into t4 gpu

---
+++ vllm_server/start_ministral8b.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Model name is the first argument
+model_name=mistralai/Ministral-3-8B-Instruct-2512
+hf_token_str=$1
+
+sudo docker pull vllm/vllm-openai:v0.15.1 && \
+    export HF_TOKEN=$hf_token_str && \
+    sudo docker run --privileged --runtime nvidia --gpus all \
+    -v ~/.cache/huggingface:/root/.cache/huggingface \
+    -e "HF_TOKEN=$HF_TOKEN" \
+    -e "VLLM_LOGGING_LEVEL=DEBUG" \
+    -p 8000:8000 \
+    --ipc=host \
+    vllm/vllm-openai:v0.15.1 \
+    --model $model_name \
+    --max-model-len 6500 \
+    --max-num-seqs 12 \
+    --gpu-memory-utilization 0.85
+    
