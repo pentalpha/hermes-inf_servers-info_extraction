a3ef5e82cca07e56a266c0ff2c81e6cd35e4466d
Pitágoras Alves <alves.pitagoras@gmail.com>
2026-06-13 20:50:32 -0300

    Adicionando manuscrito inicial

--- testing_bench/make_plots.py
+++ testing_bench/make_plots.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib.ticker as mticker
+import matplotlib.colors as mcolors
 
 df_path = "results/df_simples.csv"
 
 # Markers for each model type (only filled formats)
 model_type_to_format = {
     "Gliner v1": "o",
-    "Gliner v2": "s",
-    "Geração de Texto - Abertos": "h",
-    "Geração de Texto - Comerciais": "p",
+    "Gliner v2": "X",
+    "Geração de Texto - Abertos": "^",
+    "Geração de Texto - Comerciais": "*",
 }
 
 model_type_to_colors = {
@@ -254,6 +255,155 @@ def plot_cost_vs_latency_vs_quality(df):
     plt.close()
 
 
+def plot_cost_vs_latency_vs_quality2(df):
+    """
+    X axis is quality, Y axis is cost, color is latency
+    Fast should be green, slow should be red
+    """
+    fig, ax = plt.subplots(figsize=(12, 7.5))
+
+    # df_not_gliners = df[(df["Tipo de Modelo"] != "Gliner v1") & (df["Tipo de Modelo"] != "Gliner v2") & (df["Tipo de Modelo"] != "Others")]
+    # df_not_gliners = df[~df["Tipo de Modelo"].str.contains("Gliner")]
+    # df = pd.concat([df_gliners, df_not_gliners])
+
+    # keep onlythe top 18 results, using general quality
+
+    tipos_bons = df["Tipo de Modelo"].unique()
+    print(tipos_bons)
+
+    # Predefine colors in RdBu scale, to be able to plot types separatly,
+    # but only from smaller quality to larger quality
+    low = df["seconds_per_transcript"].min()
+    high = df["seconds_per_transcript"].max()
+
+    # Make custom scale
+    # norm = plt.Normalize(low, high)
+    norm = mcolors.LogNorm(low, high)
+    cmap = plt.cm.jet
+
+    # Invert the colormap
+    # cmap = cmap.reversed()
+
+    df["color"] = df["seconds_per_transcript"].apply(lambda x: cmap(norm(x)))
+
+    df_gliners = df[df["Tipo de Modelo"] == "Gliner v1"]
+    df_not_gliner = df[df["Tipo de Modelo"] != "Gliner v1"]
+
+    df_to_name = pd.concat([df_gliners.nlargest(5, "Qualidade Geral"), df_not_gliner])
+
+    for model_type, type_df in df.groupby("Tipo de Modelo"):
+        scatter = ax.scatter(
+            type_df["Qualidade Geral"],
+            type_df["cost_per_transcript"],
+            c=type_df["color"],
+            marker=model_type_to_format[model_type],
+            label=model_type,
+            linewidths=0,
+            s=190,
+            alpha=1,
+        )
+
+    for model_type, type_df in df_to_name.groupby("Tipo de Modelo"):
+        scatter = ax.scatter(
+            type_df["Qualidade Geral"],
+            type_df["cost_per_transcript"],
+            c=type_df["color"],
+            marker=model_type_to_format[model_type],
+            label=model_type,
+            linewidths=1,
+            edgecolors="black",
+            s=230,
+            alpha=1,
+        )
+
+    for index, row in df.iterrows():
+        print(row["Tipo de Modelo"], row["subtype"], row["Qualidade Geral"])
+
+    ax.set_xlabel("Qualidade Geral")
+    ax.set_ylabel("Custo por Transcrição ($)")
+    ax.set_title("Custo-Benefício de Modelos de Extração de Informação")
+
+    # ax.set_xscale("log")
+    ax.set_yscale("log")
+
+    formatter = mticker.ScalarFormatter()
+    formatter.set_scientific(False)
+    # Optional: Set a limit for when scientific notation should be used (e.g., set to (0,0) to include all numbers)
+    # formatter.set_powerlimits((0, 0))
+
+    # 5. Apply the formatter to both major and minor ticks for the x and y axes
+    # ax.xaxis.set_major_formatter(formatter)
+    # ax.xaxis.set_minor_formatter(formatter)
+    ax.yaxis.set_major_formatter(formatter)
+    # ax.yaxis.set_minor_formatter(formatter)
+
+    # Add colorbar to figure, but using the custom scale
+    # Cria um objeto mapeável explicitamente com as suas regras de cor
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])  # Evita warnings em algumas versões do Matplotlib
+
+    # Adiciona a barra de cores usando o ScalarMappable e os valores reais para os ticks
+    # mid = (low + high) / 2
+    points_locs = [low, 0.5, 1, 2, 4, 8, 16, high]
+    cbar = fig.colorbar(sm, ax=ax, ticks=points_locs)
+    cbar.ax.minorticks_off()
+
+    cb_ticklabels = [f"{x:.2f}" for x in points_locs]
+
+    cbar.set_label("Tempo por Transcrição (s)")
+    cbar.ax.set_yticklabels(cb_ticklabels)
+
+    # Legend outside plot of model types, with custom markers
+    # Placed below graph to not overlap colorbar
+    legend_elements = []
+    for model_type, marker in model_type_to_format.items():
+        if model_type not in tipos_bons:
+            continue
+        legend_elements.append(
+            plt.Line2D(
+                [0],
+                [0],
+                marker=marker,
+                color="w",
+                label=model_type,
+                markerfacecolor="k",
+                markersize=8,
+            )
+        )
+    ax.legend(
+        handles=legend_elements, bbox_to_anchor=(0.5, -0.1), loc="upper center", ncol=5
+    )
+
+    import matplotlib.patheffects as patheffects
+
+    # Add text labels for all results
+    for i, row in df_to_name.iterrows():
+        # Centered text, semi-transparent, small font
+        annot_point = (row["Qualidade Geral"], row["cost_per_transcript"])
+        text_point = (0, 0)
+        ax.annotate(
+            row["subtype"],
+            annot_point,
+            # xytext=text_point,
+            # textcoords="offset pixels",
+            # arrowprops={"arrowstyle": "simple"},
+            ha="center",
+            va="center",
+            fontsize=8,
+            alpha=0.85,
+            path_effects=[
+                patheffects.withStroke(
+                    linewidth=2, foreground="white", capstyle="round", alpha=0.75
+                )
+            ],
+        )
+
+    # Save plot
+    plt.savefig("results/cost_vs_latency_vs_quality2.png", dpi=400, bbox_inches="tight")
+    plt.savefig("results/cost_vs_latency_vs_quality2.svg", dpi=400, bbox_inches="tight")
+    plt.close()
+
+
 def ner_vs_clf_quality(df):
     # Scatter all the results, name all
     # With the exception of gliner models: name only the top 3 and gliner v2
@@ -406,9 +556,10 @@ if __name__ == "__main__":
     df = simple_hf_name(df)
     df = model_subtype(df)
 
-    plot_cost_vs_latency_vs_quality(df)
-    ner_vs_clf_quality(df)
-    cost_benefit(df)
+    # plot_cost_vs_latency_vs_quality(df)
+    plot_cost_vs_latency_vs_quality2(df)
+    # ner_vs_clf_quality(df)
+    # cost_benefit(df)
 
     print("Modelos por tipo:", df["Tipo de Modelo"].unique())
     for model_type, type_df in df.groupby("Tipo de Modelo"):
